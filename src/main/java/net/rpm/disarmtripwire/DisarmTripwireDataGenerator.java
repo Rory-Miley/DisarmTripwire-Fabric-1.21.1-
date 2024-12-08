b4140069008e74b0d9663dda233a7e5d129052bf
Rory-Miley <rorypmiley@gmail.com>
2024-12-07 22:41:05 -0500

    Git init

---
+++ src/main/java/net/rpm/disarmtripwire/DisarmTripwireDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rpm.disarmtripwire;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DisarmTripwireDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
