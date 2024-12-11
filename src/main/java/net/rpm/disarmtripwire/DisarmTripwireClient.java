a4daea5bb6718e43ac7917b307f4ae00ff77448a
Rory-Miley <rorypmiley@gmail.com>
2024-12-10 23:36:59 -0500

    I think i added the bug back, need to make dooper to test

---
+++ src/main/java/net/rpm/disarmtripwire/DisarmTripwireClient.java
@@ -0,0 +1,10 @@
+package net.rpm.disarmtripwire;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DisarmTripwireClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
